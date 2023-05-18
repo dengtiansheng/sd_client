dc26c444c43dc171df8f3ad759de5524585094a6
dengtiansheng <dengtiansheng@gmail.com>
2023-05-18 20:59:53 +0800

    first commit

---
+++ conf/conf.py
@@ -0,0 +1,11 @@
+MODEL_SETTINGS = {
+	1000:{
+	"model_name":"stable diffsion 1.5",
+	"type":"txt2img",
+	"model_description":"",
+	"hosts":[
+	"112.83.192.109:78619"
+	]}
+}
+
+STATUS_MODEL_ID_NOT_EXIST = {"code":4001,"detail":"model_id 不存在,请确认是否有效"}
---
+++ main.py
@@ -0,0 +1,71 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import json
+from conf import conf
+import random
+
+
+
+
+class Item(BaseModel):
+	model_id: int | None = 1000
+	prompt: str
+	negative_prompt: str | None = ""
+	seed: int | None = -1 
+	width: int | None = 512
+	height: int | None = 512
+
+	def to_dict(self):
+		return {
+			"model_id" : self.model_id,
+			"prompt" : self.prompt,
+			"negative_prompt" : self.negative_prompt,
+			"seed" : self.seed,
+			"width" : self.width,
+			"height" : self.height
+		}
+
+
+def submit_post(url: str, item: Item):
+	return requests.post(url, json.dumps(item.to_dict()))
+
+def check_exist(model_id : int):
+	return model_id in MODEL_SETTINGS
+
+def random_route(model_id : int):
+	model =  conf.MODEL_SETTINGS[model_id]
+	instance_num = len(model['hosts'])
+	index = random.randint(0,instance_num - 1) 
+	url = "http://"+model['hosts'][index]+"/sdapi/v1/"+model['type']
+	return url
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+	return {"welcome": "glad to see you"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+	return {"item_id": item_id, "q": q}
+
+@app.post("/v1/text2img")
+def text2img(item:Item):
+	res = {}
+	model_id = item.model_id
+
+	if check_exist == False:
+		res["msg"] = STATUS_MODEL_ID_NOT_EXIST
+		return res
+
+	txt2img_url = random_route(model_id)
+	#txt2img_url = 'http://112.83.192.109:7861/sdapi/v1/txt2img'
+	response = submit_post(txt2img_url, item).json()
+	image_base64 = response['images']
+	res = {"data":{"image_base64": image_base64[0],"detail": ""}}
+	return res
